12c494d03626b083fae9d9acf42549622312b7aa
Volodymyr Vlasenko <wvlasenko@gmail.com>
2021-02-01 07:11:10 +0200

    Restore

---
+++ src/service/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+const API_KEY = '19799176-4c87c41e9b3623f80b0424cac';
+const BASE_URL = 'https://pixabay.com/api';
+
+axios.defaults.baseURL = BASE_URL;
+
+axios.defaults.params = {
+  key: API_KEY,
+  image_type: 'photo',
+  orientation: 'horizontal',
+  per_page: 12,
+};
+const getImage = async (keyword = '', page) => {
+  try {
+    const { data } = await axios.get('', {
+      params: { q: keyword, page: page },
+    });
+    return data.hits;
+  } catch (error) {
+    console.log('error', { error });
+    return [];
+  }
+};
+
+export default getImage;
